436e2bbe52f386f63fd85369e13ffd421a9f6dc0
Evan Burt <kurohai@gmail.com>
2020-11-04 04:10:50 -0600

    more updates

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 
-from lib.logutil import get_logger
-from lib import LogitechKeyboard
-from lib.settings import g510
+from src.logutil import get_logger
+from src import LogitechKeyboard
+from src.settings import g510
 import sys
 
 log = get_logger(__name__)
@@ -16,11 +16,11 @@ def main():
         log.error(e)
         sys.exit(0)
 
-    log.info('logi-nixd running: {0}'.format(lgkb.running))
+        log.info('logi-nixd running: {0}'.format(lgkb.running))
 
-    lgkb.start_event_handling()
+        lgkb.start_event_handling()
 
-    log.info(str(lgkb.controller.keyboard))
+        log.info(str(lgkb.controller.keyboard))
 
 
 if __name__ == '__main__':
